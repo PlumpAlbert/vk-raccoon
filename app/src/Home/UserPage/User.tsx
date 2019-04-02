c7252d116bdd1e6f06a8b235fe094cec0472d154
plump_albert <plumpalbert@gmail.com>
2019-04-02 22:20:01 +0300

    Created first draft of User page

--- app/src/Home/UserPage/User.tsx
+++ app/src/Home/UserPage/User.tsx
@@ -20,20 +20,21 @@ const User: React.FC<IProps> = props => {
   } = props.user;
   let friendButton = null;
   if (friend_status) {
-    friendButton = <button> {
+    friendButton = <button className='action-btn'> {
       friend_status === FriendStatus.Friend ? "Remove from friend list" : "Add to friend list"
     } </button>;
   }
   return (
     <div className='user'>
       <div className="user-info">
-        <div className="user-wrapper">
+        <div className='user-wrapper'>
           <img src={photo_400_orig} alt="UserPhoto" />
-          <h1>{`${first_name} ${maiden_name ? `(${maiden_name}) ${last_name}` : last_name}`}</h1>
+          <h3>{`${first_name} ${maiden_name ? `(${maiden_name}) ${last_name}` : last_name}`}</h3>
           <blockquote>{status}</blockquote>
-          {can_write_private_message ? <button>Write Message</button> : null}
+          {can_write_private_message ? <button className='action-btn'>Write Message</button> : null}
           {can_send_friend_request || friend_status === FriendStatus.Friend ? friendButton : null}
           {friend_status === FriendStatus.OutRequest ? <span>{first_name} sent you a friend request</span> : null}
+          <span className="about">More information ></span>
         </div>
       </div>
       <div className="user-content"></div>
