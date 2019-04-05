fb8e39ef80b5e43d293a0e16e5c3606e2a80d060
plump_albert <plumpalbert@gmail.com>
2019-04-05 09:08:57 +0300

    Updated Post rendering

--- app/src/Post/Post.tsx
+++ app/src/Post/Post.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { IResponse } from '../API/wall/get';
-import { IPost, IAttachment, AttachmentType, SizeType } from '../API/objects';
+import { IPost, IAttachment, AttachmentType, IPhotoAttachment, IAudioAttachment } from '../API/objects';
 import './Post.css'
 
 class Post extends React.Component<IResponse> {
@@ -106,24 +106,169 @@ class Post extends React.Component<IResponse> {
     )
   }
   renderAttachments = (data: IAttachment[]) => {
-    return data.map((item, ix) => {
-      switch (item.type) {
-        case AttachmentType.photo: {
-          if (item.photo === undefined) return console.error('item.photo is not defined', item);
-          let photo = item.photo.sizes[item.photo.sizes.length - 1];
-          return <img
-            key={item.photo.access_key}
-            src={photo.url}
-            onClick={(e) => {
-              e.preventDefault();
-              console.log(photo);
-            }}
-          />;
+    let audio = this.renderAudios(
+      data.filter(item => item.type === AttachmentType.audio ? item : null) as {
+        type: AttachmentType.audio,
+        audio: IAudioAttachment
+      }[]
+    );
+    let photos = this.renderPhotos(
+      data.filter(item => item.type === AttachmentType.photo ? item : null) as {
+        type: AttachmentType.photo,
+        photo: IPhotoAttachment
+      }[]
+    );
+    return [photos].concat(audio);
+  }
+  renderPhotos = (photos: { type: AttachmentType.photo, photo: IPhotoAttachment }[]) => {
+    switch (photos.length) {
+      case 1:
+      case 2:
+      case 3:
+      case 4: {
+        let left = 1, right = 0;
+        switch (photos.length) {
+          case 2: { right = 1; } break;
+          case 3: { right = 2; } break;
+          case 4: { left = right = 2; } break;
+          default: break;
         }
-        default:
-          return <span>{item.type}</span>
+        return (
+          <table className="photo-attachment">
+            <tbody>
+              <tr>
+                <td>{
+                  (function () {
+                    let nodes = [];
+                    for (let i = 0; i < left; i++) {
+                      let photo = photos[i].photo;
+                      nodes.push(<img
+                        key={photo.access_key}
+                        src={photo.sizes[photo.sizes.length - 1].url}
+                        onClick={(e) => {
+                          e.preventDefault();
+                          console.log(photo);
+                        }}
+                      />);
+                    }
+                    return nodes;
+                  })()
+                }</td>
+                {
+                  right > 0
+                    ? <td>{
+                      (function () {
+                        let nodes = [];
+                        for (let i = 0; i < right; i++) {
+                          let photo = photos[i * left + right].photo;
+                          nodes.push(<img
+                            key={photo.access_key}
+                            src={photo.sizes[photo.sizes.length - 1].url}
+                            onClick={(e) => {
+                              e.preventDefault();
+                              console.log(photo);
+                            }}
+                          />);
+                        }
+                        return nodes;
+                      })()
+                    }</td>
+                    : null
+                }
+              </tr>
+            </tbody>
+          </table>
+        );
       }
-    })
+      case 5:
+      case 6:
+      case 7:
+      case 8:
+      case 9:
+      case 10: {
+        let up = 2, down = 3;
+        switch (photos.length) {
+          case 6: { up = 3; } break;
+          case 7: { up = 3; down = 4 } break;
+          case 8: { up = 4; down = 4 } break;
+          case 9: { up = 2; down = 7; } break;
+          case 10: { up = 2; down = 8; } break;
+          default: break;
+        }
+        return (
+          <table className="photo-attachment">
+            <tbody>
+              <tr>
+                {
+                  (function () {
+                    let nodes = [];
+                    for (let i = 0; i < up; i++) {
+                      let photo = photos[i].photo;
+                      nodes.push(
+                        <td>
+                          <img
+                            key={photo.access_key}
+                            src={photo.sizes[photo.sizes.length - 1].url}
+                            onClick={(e) => {
+                              e.preventDefault();
+                              console.log(photo);
+                            }}
+                          />
+                        </td>
+                      );
+                    }
+                    return nodes;
+                  })()
+                }
+              </tr>
+              <tr>
+                {
+                  (function () {
+                    let nodes = [];
+                    for (let i = 0; i < down; i++) {
+                      let photo = photos[up + i].photo;
+                      nodes.push(
+                        <td>
+                          <img
+                            key={photo.access_key}
+                            src={photo.sizes[photo.sizes.length - 1].url}
+                            onClick={(e) => {
+                              e.preventDefault();
+                              console.log(photo);
+                            }}
+                          />
+                        </td>
+                      );
+                    }
+                    return nodes;
+                  })()
+                }
+              </tr>
+            </tbody>
+          </table>
+        );
+      }
+      default: return null;
+    }
+  }
+  renderAudios = (audios: { type: AttachmentType.audio, audio: IAudioAttachment }[]) => {
+    return (
+      <div className="audio-list">
+        {
+          audios.map(({ type, audio }) => {
+            return (
+              <div className='track'>
+                <span className='fa fa-play' />
+                <div className='track-info'>
+                  <h1 className='track-title'>{audio.title}</h1>
+                  <p className='track-artist'>{audio.artist}</p>
+                </div>
+              </div>
+            );
+          })
+        }
+      </div>
+    );
   }
 }
 
