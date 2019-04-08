33b7d2ab5799bf543928a2e939b0a0235b8aa7fc
plump_albert <plumpalbert@gmail.com>
2019-04-08 23:28:28 +0300

    Fixed scrolling problem

--- app/src/Post/index.tsx
+++ app/src/Post/index.tsx
@@ -1,11 +1,11 @@
-import * as React from 'react'
-import { IParams } from '../API/wall/get';
-import Post from './Post';
-import API from '../API';
-import { IGroup, IUser } from '../API/objects';
-import createLog from '../logging';
+import * as React from "react";
+import { IParams } from "../API/wall/get";
+import Post from "./Post";
+import API from "../API";
+import { IGroup, IUser } from "../API/objects";
+import createLog from "../logging";
 
-const log = createLog('Posts');
+const log = createLog("Posts");
 
 interface IProps {
   token: string;
@@ -26,51 +26,56 @@ class Posts extends React.Component<IProps, IState> {
     postCount: 0,
     end: false,
     posts: []
-  }
+  };
   componentWillMount() {
     let { token, userId } = this.props;
     this.setState({ loading: true });
-    let req: IParams = {
-      token,
-      owner_id: userId,
-      count: 10,
-      extended: true,
-      fields: [
-        "photo_100"
-      ]
-    };
-    API.wall.get(req).then(res => {
-      log('Fetched initial posts');
-      this.setState({
-        loading: false,
-        offset: 10,
-        postCount: 10,
-        end: res.count <= 10,
-        posts: res.items.map(post => {
-          let sources: any[] = [];
-          if (post.copy_history) {
-            sources = post.copy_history.map(repost => {
-              if (repost.from_id >= 0)
+    API.wall
+      .get({
+        token,
+        owner_id: userId,
+        count: 10,
+        extended: true,
+        fields: ["photo_100"]
+      })
+      .then(res => {
+        log("Fetched initial posts", res.items);
+        this.setState({
+          loading: false,
+          offset: res.items.length + 1,
+          postCount: res.items.length,
+          end: res.count === res.items.length,
+          posts: res.items.map(post => {
+            let sources: any[] = [];
+            if (post.copy_history) {
+              sources = post.copy_history.map(repost => {
+                if (repost.from_id >= 0)
+                  return {
+                    type: "IUser",
+                    data: res.profiles.find(
+                      user => user.id === repost.from_id
+                    ) as IUser
+                  };
                 return {
-                  type: 'IUser',
-                  data: res.profiles.find(user => user.id === repost.from_id) as IUser
+                  type: "IGroup",
+                  data: res.groups.find(
+                    group => group.id === -repost.from_id
+                  ) as IGroup
                 };
-              return {
-                type: 'IGroup',
-                data: res.groups.find(group => group.id === -repost.from_id) as IGroup
-              };
-            });
-          }
-          return <Post
-            key={post.id}
-            data={post}
-            sources={sources}
-            user={res.profiles.find(user => user.id === post.from_id)}
-            group={res.groups.find(group => group.id === post.from_id)}
-          />
-        })
+              });
+            }
+            return (
+              <Post
+                key={post.id}
+                data={post}
+                sources={sources}
+                user={res.profiles.find(user => user.id === post.from_id)}
+                group={res.groups.find(group => group.id === post.from_id)}
+              />
+            );
+          })
+        });
       });
-    });
   }
   shouldComponentUpdate(newProps: IProps, newState: IState) {
     let { loading: newLoading, ...newStateProps } = newState;
@@ -81,50 +86,47 @@ class Posts extends React.Component<IProps, IState> {
       for (let key in prevS) {
         if (prevS[key] !== newS[key]) {
           log(
-            'props/state had changed\n',
-            'newProps =', newProps, '\n',
-            'newState =', newState, '\n'
+            "props/state had changed\n",
+            "newState =", newState, "\n",
+            "prevState =", this.state, "\n"
           );
           return true;
-        };
+        }
       }
       return false;
-    }
-    else if (newState !== this.state) {
+    } else if (newState !== this.state) {
       log(
-        'props/state had changed\n',
-        'newProps =', newProps, '\n',
-        'newState =', newState, '\n'
+        "props/state had changed\n",
+        "newState =", newState, "\n",
+        "prevState =", this.state, "\n"
       );
       return true;
     }
     return false;
   }
   componentDidUpdate() {
-    let node = document.querySelector('div.post-wrapper');
+    let node = document.querySelector("div.post-wrapper");
     if (node) {
-      node.scrollIntoView({ block: 'center' });
-      let posts = node.getElementsByClassName('post');
-      const maxScroll = node.scrollHeight - posts[posts.length - 1].clientHeight;
+      node.scrollIntoView({ block: "center" });
+      let posts = node.getElementsByClassName("post");
+      const maxScroll =
+        node.scrollHeight - posts[posts.length - 1].clientHeight;
       if (node.clientHeight <= maxScroll / 2) return;
       this.setState({ loading: true });
       API.wall.get({
         token: this.props.token,
         owner_id: this.props.userId,
         count: 5,
-        offset: this.state.offset + 1,
+        offset: this.state.offset,
         extended: true,
-        fields: [
-          "photo_100"
-        ]
+        fields: ["photo_100"]
       }).then(res => {
-        log('Appending initial posts', res.items);
-
+        log("Appending initial posts", res.items);
         this.setState({
           offset: this.state.offset + res.items.length,
-          postCount: this.state.postCount + 5,
+          postCount: this.state.postCount + res.items.length,
           loading: false,
-          end: res.count === this.state.offset + res.items.length,
+          end: res.count === this.state.offset + res.items.length - 1,
           posts: [
             ...this.state.posts,
             ...res.items.map(post => {
@@ -133,25 +135,31 @@ class Posts extends React.Component<IProps, IState> {
                 sources = post.copy_history.map(repost => {
                   if (repost.from_id >= 0)
                     return {
-                      type: 'IUser',
-                      data: res.profiles.find(user => user.id === repost.from_id) as IUser
+                      type: "IUser",
+                      data: res.profiles.find(
+                        user => user.id === repost.from_id
+                      ) as IUser
                     };
                   return {
-                    type: 'IGroup',
-                    data: res.groups.find(group => group.id === -repost.from_id) as IGroup
+                    type: "IGroup",
+                    data: res.groups.find(
+                      group => group.id === -repost.from_id
+                    ) as IGroup
                   };
                 });
               }
-              return <Post
-                key={post.id}
-                data={post}
-                sources={sources}
-                user={res.profiles.find(user => user.id === post.from_id)}
-                group={res.groups.find(group => group.id === post.from_id)}
-              />
+              return (
+                <Post
+                  key={post.id}
+                  data={post}
+                  sources={sources}
+                  user={res.profiles.find(user => user.id === post.from_id)}
+                  group={res.groups.find(group => group.id === post.from_id)}
+                />
+              );
             })
           ]
-        })
+        });
       });
     }
   }
@@ -159,55 +167,8 @@ class Posts extends React.Component<IProps, IState> {
     if (this.state.loading) return;
     let { offset, postCount } = this.state;
     const target = e.currentTarget;
-    let posts = target.getElementsByClassName('post');
+    let posts = target.getElementsByClassName("post");
     const maxScroll = target.scrollHeight - posts[posts.length - 1].clientHeight;
-    if (target.clientHeight > maxScroll / 2) {
-      this.setState({ loading: true });
-      return API.wall.get({
-        token: this.props.token,
-        owner_id: this.props.userId,
-        count: 5,
-        offset: offset + 1,
-        extended: true,
-        fields: [
-          "photo_100"
-        ]
-      }).then(res => {
-        log('Appending initial posts', res.items);
-        this.setState({
-          offset: offset + res.items.length,
-          postCount: this.state.postCount + 5,
-          loading: false,
-          end: res.count === offset + res.items.length,
-          posts: [
-            ...this.state.posts,
-            ...res.items.map(post => {
-              let sources: any[] = [];
-              if (post.copy_history) {
-                sources = post.copy_history.map(repost => {
-                  if (repost.from_id >= 0)
-                    return {
-                      type: 'IUser',
-                      data: res.profiles.find(user => user.id === repost.from_id) as IUser
-                    };
-                  return {
-                    type: 'IGroup',
-                    data: res.groups.find(group => group.id === -repost.from_id) as IGroup
-                  };
-                });
-              }
-              return <Post
-                key={post.id}
-                data={post}
-                sources={sources}
-                user={res.profiles.find(user => user.id === post.from_id)}
-                group={res.groups.find(group => group.id === post.from_id)}
-              />
-            })
-          ]
-        })
-      });
-    }
     let scrollHeight = target.clientHeight + target.scrollTop;
     if (scrollHeight > maxScroll) {
       if (this.state.end) return;
@@ -216,13 +177,11 @@ class Posts extends React.Component<IProps, IState> {
         token: this.props.token,
         owner_id: this.props.userId,
         count: 5,
-        offset: offset + 1,
+        offset,
         extended: true,
-        fields: [
-          "photo_100"
-        ]
+        fields: ["photo_100"]
       }).then(res => {
-        log('Fetched new posts', res.items);
+        log("Fetched new posts", res.items);
         this.setState({
           posts: [
             ...this.state.posts.slice(res.items.length),
@@ -232,43 +191,48 @@ class Posts extends React.Component<IProps, IState> {
                 sources = post.copy_history.map(repost => {
                   if (repost.from_id >= 0)
                     return {
-                      type: 'IUser',
-                      data: res.profiles.find(user => user.id === repost.from_id) as IUser
+                      type: "IUser",
+                      data: res.profiles.find(
+                        user => user.id === repost.from_id
+                      ) as IUser
                     };
                   return {
-                    type: 'IGroup',
-                    data: res.groups.find(group => group.id === -repost.from_id) as IGroup
+                    type: "IGroup",
+                    data: res.groups.find(
+                      group => group.id === -repost.from_id
+                    ) as IGroup
                   };
                 });
               }
-              return <Post
-                key={post.id}
-                data={post}
-                sources={sources}
-                user={res.profiles.find(user => user.id === post.from_id)}
-                group={res.groups.find(group => group.id === post.from_id)}
-              />
+              return (
+                <Post
+                  key={post.id}
+                  data={post}
+                  sources={sources}
+                  user={res.profiles.find(user => user.id === post.from_id)}
+                  group={res.groups.find(group => group.id === post.from_id)}
+                />
+              );
             })
           ],
           offset: offset + res.items.length,
           loading: false,
-          end: res.count === offset + res.items.length
+          end: res.count === offset + res.items.length - 1
         });
       });
     }
-    if (target.scrollTop < posts[0].clientHeight && offset > postCount) {
+    if (target.scrollTop < posts[0].clientHeight && offset > postCount + 1) {
       this.setState({ loading: true });
+      let count = offset - postCount < 5 ? offset - postCount : 5;
       API.wall.get({
         token: this.props.token,
         owner_id: this.props.userId,
-        count: 5,
-        offset: offset - postCount - 5 > 0 ? offset - postCount - 5 : 0,
+        count,
+        offset: offset - postCount - count,
         extended: true,
-        fields: [
-          "photo_100"
-        ]
+        fields: ["photo_100"]
       }).then(res => {
-        log('Fetched previous posts', res.items);
+        log("Fetched previous posts", res.items);
         this.setState({
           posts: [
             ...res.items.map(post => {
@@ -277,41 +241,45 @@ class Posts extends React.Component<IProps, IState> {
                 sources = post.copy_history.map(repost => {
                   if (repost.from_id >= 0)
                     return {
-                      type: 'IUser',
-                      data: res.profiles.find(user => user.id === repost.from_id) as IUser
+                      type: "IUser",
+                      data: res.profiles.find(
+                        user => user.id === repost.from_id
+                      ) as IUser
                     };
                   return {
-                    type: 'IGroup',
-                    data: res.groups.find(group => group.id === -repost.from_id) as IGroup
+                    type: "IGroup",
+                    data: res.groups.find(
+                      group => group.id === -repost.from_id
+                    ) as IGroup
                   };
                 });
               }
-              return <Post
-                key={post.id}
-                data={post}
-                sources={sources}
-                user={res.profiles.find(user => user.id === post.from_id)}
-                group={res.groups.find(group => group.id === post.from_id)}
-              />
+              return (
+                <Post
+                  key={post.id}
+                  data={post}
+                  sources={sources}
+                  user={res.profiles.find(user => user.id === post.from_id)}
+                  group={res.groups.find(group => group.id === post.from_id)}
+                />
+              );
             }),
-            ...this.state.posts.slice(0, res.items.length)
+            ...this.state.posts.slice(0, postCount - res.items.length)
           ],
-          offset: offset - res.items.length,
+          offset: offset - res.items.length - 1,
           loading: false,
           end: false
         });
       });
     }
-  }
+  };
   render() {
     let { posts } = this.state;
-    log('Rendering');
+    log("Rendering");
     return (
-      <div className='post-wrapper' onScroll={this.onScroll}>{
-        posts.length === 0
-          ? <span>Loading...</span>
-          : posts
-      }</div>
+      <div className="post-wrapper" onScroll={this.onScroll}>
+        {posts.length === 0 ? <span>Loading...</span> : posts}
+      </div>
     );
   }
 }
