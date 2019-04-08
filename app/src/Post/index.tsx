f478336de90ba6465de876a370d52a536e00d79f
plump_albert <plumpalbert@gmail.com>
2019-04-08 10:54:15 +0300

    Fixed scrolling issue

--- app/src/Post/index.tsx
+++ app/src/Post/index.tsx
@@ -11,14 +11,16 @@ interface IProps {
 interface IState {
   loading: boolean;
   offset: number;
+  postCount: number;
   end: boolean;
-  posts: JSX.Element[]
+  posts: JSX.Element[];
 }
 
 class Posts extends React.PureComponent<IProps, IState> {
   state: IState = {
     loading: false,
     offset: 0,
+    postCount: 0,
     end: false,
     posts: []
   }
@@ -35,10 +37,11 @@ class Posts extends React.PureComponent<IProps, IState> {
       ]
     };
     API.wall.get(req).then(res => {
-      console.log('#POSTS > Fetched first posts', new Date(Date.now()).toLocaleTimeString());
+      console.log('#POSTS > Fetched initial posts', new Date(Date.now()).toLocaleTimeString());
       this.setState({
         loading: false,
         offset: 10,
+        postCount: 10,
         end: res.count <= 10,
         posts: res.items.map(post => {
           let sources: any[] = [];
@@ -66,14 +69,130 @@ class Posts extends React.PureComponent<IProps, IState> {
       });
     });
   }
+  shouldComponentUpdate(newProps: IProps, newState: IState) {
+    console.log('#POST > props/state has changed', newProps, newState);
+    let { loading: newLoading, ...newStateProps } = newState;
+    let { loading: prevLoading, ...prevStateProps } = this.state;
+    if (newLoading !== prevLoading) {
+      let prevS = prevStateProps as any;
+      let newS = newStateProps as any;
+      for (let key in prevS) {
+        if (prevS[key] !== newS[key]) return true;
+      }
+      return false;
+    }
+    return true;
+  }
+  componentDidUpdate() {
+    let node = document.querySelector('div.post-wrapper');
+    if (node) {
+      node.scrollIntoView({ block: 'center' });
+      let posts = node.getElementsByClassName('post');
+      const maxScroll = node.scrollHeight - posts[posts.length - 1].clientHeight;
+      if (node.clientHeight <= maxScroll / 2) return;
+      this.setState({ loading: true });
+      API.wall.get({
+        token: this.props.token,
+        owner_id: this.props.userId,
+        count: 5,
+        offset: this.state.offset + 1,
+        extended: true,
+        fields: [
+          "photo_100"
+        ]
+      }).then(res => {
+        console.log('#POSTS > Appending initial posts', res.items, new Date(Date.now()).toLocaleTimeString());
+        this.setState({
+          offset: this.state.offset + res.items.length,
+          postCount: this.state.postCount + 5,
+          loading: false,
+          end: res.count === this.state.offset + res.items.length,
+          posts: [
+            ...this.state.posts,
+            ...res.items.map(post => {
+              let sources: any[] = [];
+              if (post.copy_history) {
+                sources = post.copy_history.map(repost => {
+                  if (repost.from_id >= 0)
+                    return {
+                      type: 'IUser',
+                      data: res.profiles.find(user => user.id === repost.from_id) as IUser
+                    };
+                  return {
+                    type: 'IGroup',
+                    data: res.groups.find(group => group.id === -repost.from_id) as IGroup
+                  };
+                });
+              }
+              return <Post
+                key={post.id}
+                data={post}
+                sources={sources}
+                user={res.profiles.find(user => user.id === post.from_id)}
+                group={res.groups.find(group => group.id === post.from_id)}
+              />
+            })
+          ]
+        })
+      });
+    }
+  }
   onScroll = (e: React.UIEvent<HTMLDivElement>) => {
     if (this.state.loading) return;
-    if (this.state.end) return;
-    let { offset } = this.state;
-    let target = e.currentTarget;
+    let { offset, postCount } = this.state;
+    const target = e.currentTarget;
     let posts = target.getElementsByClassName('post');
+    const maxScroll = target.scrollHeight - posts[posts.length - 1].clientHeight;
+    if (target.clientHeight > maxScroll / 2) {
+      this.setState({ loading: true });
+      return API.wall.get({
+        token: this.props.token,
+        owner_id: this.props.userId,
+        count: 5,
+        offset: offset + 1,
+        extended: true,
+        fields: [
+          "photo_100"
+        ]
+      }).then(res => {
+        console.log('#POSTS > Appending initial posts', res.items, new Date(Date.now()).toLocaleTimeString());
+        this.setState({
+          offset: offset + res.items.length,
+          postCount: this.state.postCount + 5,
+          loading: false,
+          end: res.count === offset + res.items.length,
+          posts: [
+            ...this.state.posts,
+            ...res.items.map(post => {
+              let sources: any[] = [];
+              if (post.copy_history) {
+                sources = post.copy_history.map(repost => {
+                  if (repost.from_id >= 0)
+                    return {
+                      type: 'IUser',
+                      data: res.profiles.find(user => user.id === repost.from_id) as IUser
+                    };
+                  return {
+                    type: 'IGroup',
+                    data: res.groups.find(group => group.id === -repost.from_id) as IGroup
+                  };
+                });
+              }
+              return <Post
+                key={post.id}
+                data={post}
+                sources={sources}
+                user={res.profiles.find(user => user.id === post.from_id)}
+                group={res.groups.find(group => group.id === post.from_id)}
+              />
+            })
+          ]
+        })
+      });
+    }
     let scrollHeight = target.clientHeight + target.scrollTop;
-    if (scrollHeight > target.scrollHeight - posts[posts.length - 1].clientHeight) {
+    if (scrollHeight > maxScroll) {
+      if (this.state.end) return;
       this.setState({ loading: true });
       API.wall.get({
         token: this.props.token,
@@ -86,13 +205,9 @@ class Posts extends React.PureComponent<IProps, IState> {
         ]
       }).then(res => {
         console.log('#POSTS > Fetched new posts', res.items, new Date(Date.now()).toLocaleTimeString());
-        let count = res.count - offset < 5
-          ? res.count - offset
-          : 5;
-        target.scrollIntoView({ block: 'center' });
         this.setState({
           posts: [
-            ...this.state.posts.slice(count),
+            ...this.state.posts.slice(res.items.length),
             ...res.items.map(post => {
               let sources: any[] = [];
               if (post.copy_history) {
@@ -117,29 +232,25 @@ class Posts extends React.PureComponent<IProps, IState> {
               />
             })
           ],
-          offset: offset + count,
+          offset: offset + res.items.length,
           loading: false,
-          end: res.count === offset + count
+          end: res.count === offset + res.items.length
         });
       });
     }
-    if (target.scrollTop < 500 && offset !== 10) {
+    if (target.scrollTop < posts[0].clientHeight && offset > postCount) {
       this.setState({ loading: true });
       API.wall.get({
         token: this.props.token,
         owner_id: this.props.userId,
         count: 5,
-        offset: offset - 15,
+        offset: offset - postCount - 5 > 0 ? offset - postCount - 5 : 0,
         extended: true,
         fields: [
           "photo_100"
         ]
       }).then(res => {
         console.log('#POSTS > Fetched previous posts', res.items, new Date(Date.now()).toLocaleTimeString());
-        let count = res.count - offset < 5
-          ? res.count - offset
-          : 5;
-        target.scrollIntoView({ block: 'center' });
         this.setState({
           posts: [
             ...res.items.map(post => {
@@ -165,9 +276,9 @@ class Posts extends React.PureComponent<IProps, IState> {
                 group={res.groups.find(group => group.id === post.from_id)}
               />
             }),
-            ...this.state.posts.slice(0, count)
+            ...this.state.posts.slice(0, res.items.length)
           ],
-          offset: offset - count,
+          offset: offset - res.items.length,
           loading: false,
           end: false
         });
