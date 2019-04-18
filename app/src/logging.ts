2d86b11897a68c9d9658557c9edcc767f879ec2d
plump_albert <plumpalbert@gmail.com>
2019-04-18 15:46:26 +0300

    Colored output for logs

--- app/src/logging.ts
+++ app/src/logging.ts
@@ -1,18 +1,26 @@
 export const Log = (componentName: string) => {
   return (...args: any[]) => {
-    args.push(new Date(Date.now()).toLocaleTimeString());
-    console.log.apply(console, [`@${componentName} >`, ...args]);
+    console.log.apply(console, [
+      `%c${componentName} @ ${new Date(Date.now()).toLocaleTimeString()} >`, 'color: MIDNIGHTBLUE',
+      ...args
+    ]);
   };
 };
 export const infoLog = (componentName: string) => {
   return (...args: any[]) => {
     args.push(new Date(Date.now()).toLocaleTimeString());
-    console.info.apply(console, [`@${componentName} >`, ...args]);
+    console.info.apply(console, [
+      `%c${componentName} @ ${new Date(Date.now()).toLocaleTimeString()} >`, 'color: DEEPSKYBLUE',
+      ...args
+    ]);
   };
 };
 export const errorLog = (componentName: string) => {
   return (...args: any[]) => {
     args.push(new Date(Date.now()).toLocaleTimeString());
-    console.error.apply(console, [`@${componentName} >`, ...args]);
+    console.error.apply(console, [
+      `%c${componentName} @ ${new Date(Date.now()).toLocaleTimeString()} >`, 'color: CRIMSON',
+      ...args
+    ]);
   };
 };
