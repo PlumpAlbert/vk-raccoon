38cb91f76dfbcda5abdb8267c761f3d83d5f1fc9
plump_albert <plumpalbert@gmail.com>
2019-05-03 19:08:22 +0300

    Updated API fetch mechanism

--- app/src/API/types.ts
+++ app/src/API/types.ts
@@ -5,6 +5,27 @@ export type TMainParams = {
   token: string;
 };
 
+interface IError {
+  /** Код ошибки */
+  error_code: number;
+  /** Сообщение об ошибке */
+  error_msg: string;
+  /** Параметры запроса, которые привели к возникновению ошибки */
+  request_params: Array<{ key: string, value: string }>
+}
+
+export class FetchError implements IError {
+  error_code: number;
+  error_msg: string;
+  request_params: Array<{ key: string, value: string }>;
+
+  constructor(obj: IError) {
+    this.error_code = obj.error_code;
+    this.error_msg = obj.error_msg;
+    this.request_params = obj.request_params;
+  }
+}
+
 export async function apiGet<IResponse>(name: string, token: string): Promise<IResponse>;
 export async function apiGet<IResponse>(name: string, token: string, fields: any): Promise<IResponse>;
 export async function apiGet<IResponse>(name: string, token: string, fields?: any) {
@@ -16,9 +37,11 @@ export async function apiGet<IResponse>(name: string, token: string, fields?: an
   }
   try {
     let response = await fetch(apiUrl + name + query);
-    return (await response.json()).response as IResponse;
+    let json = await response.json();
+    if (json.error) return new FetchError(json.error);
+    return json.response as IResponse;
   } catch (e) {
-    console.error(e);
-    return {} as IResponse;
+    console.error('Error while fetching request:', e);
+    return new FetchError({error_code: -1, error_msg: 'Fetching failed', request_params: []});
   }
 }
